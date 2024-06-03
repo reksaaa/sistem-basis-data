aceaed92bc855e5af69496b6ea5a48309d3e7bd9
reksa ariansyah <reksa.ariansyah@gmail.com>
2024-06-03 21:49:57 +0700

    pertemuan 2

--- latihan1.sql
+++ latihan1.sql
@@ -1,7 +1,53 @@
 select * from sbd.barang 
 
 
+CREATE TABLE penjualan (
+idJual int NOT NULL AUTO_INCREMENT,
+barangID int NOT NULL,
+Quantity int,
+Subtotal FLOAT,
+PRIMARY KEY (idJual),
+FOREIGN KEY (barangID) REFERENCES barang(barangID)
+);
 
-select * from sbd.barang
-where idBarang = 1
+select * from barang b 
+where b.barangStock > 5
+order by barangHarga desc
+limit 2;
+
+select * from barang
+where barangName like '%baj%'
+
+select * from barang
+
+
+insert into penjualan (barangID, Quantity, Subtotal)
+values 
+(2, 3, 900000),
+(1,4, 400000),
+(3, 6, 600000),
+(6, 2, 100000)
+;
+
+
+select * from penjualan p 
+
+select AVG(Subtotal) as "Rata2 Penjualan" from penjualan
+
+select barangID, Quantity , SUM(Quantity) , SUM(Subtotal), COUNT(barangID)
+from penjualan p 
+where p.tanggal = '2024-02-04' or p.tanggal = '2024-02-03'
+group by barangID 
+
+select * from barang b 
+where b.barangStock <= 7 and barangHarga <= 200000
+
+select p.barangID,count(barangID)
+from penjualan p 
+group by barangID 
+having count(barangID) = 1
+
+
+
+
 
