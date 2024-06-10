bcfd9385d7a32cf667f5d6beb9aa03f1087a2fce
reksa ariansyah <reksa.ariansyah@gmail.com>
2024-06-10 21:35:41 +0700

    add comment in sql query

--- latihan2.sql
+++ latihan2.sql
@@ -1,5 +1,4 @@
-
-
+-- add movie
 CREATE TABLE movie (
 idMovie CHAR(3) NOT NULL,
 title varchar(200) NOT NULL,
@@ -7,7 +6,7 @@ category varchar(100) not null,
 PRIMARY KEY (idMovie)
 );
 
-
+-- insert movie
 insert into movie (idMovie, title, category)
 values 
 ('A01', 'Kimi No Nawa', 'Animations'),
@@ -21,6 +20,7 @@ values
 ('D02', 'The Mist', 'Scifi'),
 ('E01', 'Bodyguard', 'Action');
 
+-- create table viwer
 CREATE TABLE viewer (
 idViewer int NOT NULL AUTO_INCREMENT,
 firstName varchar(100) NOT NULL,
@@ -30,6 +30,8 @@ PRIMARY KEY (idViewer),
 FOREIGN KEY (idMovie) REFERENCES movie(idMovie)
 );
 
+
+-- create table bioskop
 CREATE TABLE bioskop (
 idBioskop int NOT NULL AUTO_INCREMENT,
 nama varchar(100) NOT NULL,
@@ -38,7 +40,7 @@ PRIMARY KEY (idBioskop)
 );
 
 
-
+-- insert data viwer
 insert into viewer (firstName, lastName, idMovie)
 values 
 ('Bambang', 'Agus', 'D01'),
@@ -49,9 +51,11 @@ values
 ('Asep', 'Sukron', 'B01');
 
 
+-- cross join
 select * from movie m cross join viewer v 
 
 
+-- inner join movie dan viwer
 select v.firstName , v.lastName , m.title , m.category 
 from movie m inner join viewer v 
 on m.idMovie = v.idMovie
@@ -59,27 +63,34 @@ on m.idMovie = v.idMovie
 select * from penjualan p 
 
 
+-- inner join penjualan dan barang dengan group by dan having
 select b.barangName , SUM(p.Quantity) , SUM(p.Subtotal) 
 from penjualan p inner join barang b 
 on p.barangID = b.barangID 
 group by b.barangName 
 having b.barangName = 'Baliho'
 
+
+-- inner join alternative
 select v.firstName , v.lastName , m.title , m.category 
 from movie m, viewer v 
 where  m.idMovie = v.idMovie
 
 
+
+-- left join movie dan viewer
 select m.title , m.category , v.firstName 
 from movie m left join viewer v  
 on m.idMovie = v.idMovie 
 
 
 
+-- right join movie dan viewer
 select m.title , m.category , v.firstName , v.lastName
 from viewer v right join movie m 
 on v.idMovie = m.idMovie 
 
+-- insert data bioskop
 insert into bioskop (nama, alamat)
 values
 ('XXI Trans Studio Mall', 'Jl Gatsu no 100'),
@@ -87,6 +98,7 @@ values
 ('XXI Ciwalk', 'Jl Cihampelas no 12');
 
 
+-- 3 table join movie, viewer dan bioskop
 select v.firstName , m.title , m.category, b.nama  
 from 
 movie m left join viewer v 
